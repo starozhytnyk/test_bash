ac49d6eb0cce9ed1011607f59f6c6ed373ff7955
Dima <dima.starozhytnyk@gmail.com~>
2021-03-02 21:46:12 +0200

    I try :-)

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-echo "Some command how to know current users:"
-echo "$USER"
-echo "whoami"
-
-
-echo "some changes"
+echo "Save video...."
+echo "Initialing...."
+sleep 4
+echo "10%...."
+sleep 5
+echo "33%...."
+sleep 4
+echo "54%...."
+sleep 6
+echo "98%..."
+sleep 10
+echo "I tried but died"
